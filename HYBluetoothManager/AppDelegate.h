5d52ca898eda574e4433684f2cd6353bb5af084e
落水🐟 <970312727@qq.com>
2018-12-17 10:53:45 +0800

    Initial Commit

---
+++ HYBluetoothManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYBluetoothManager
+//
+//  Created by 徐浩宇 on 2018/12/17.
+//  Copyright © 2018 徐浩宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYBluetoothManager/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HYBluetoothManager
+//
+//  Created by 徐浩宇 on 2018/12/17.
+//  Copyright © 2018 徐浩宇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
